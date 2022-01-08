a15bbfe3e887c0d67eea44fdc091733894bde9a8
DomAitken <daitken123@hotmail.com>
2022-01-08 21:52:59 +0000

    Pets' Sounds Task

---
+++ Maven_Dep_Pets/src/main/java/Pets/Pets.java
@@ -0,0 +1,13 @@
+package Pets;
+
+public class Pets {
+
+	public void makeNoise() {
+		System.out.println("I make alot of noise and annoy the neighbours.");
+	}
+	
+	public void doThing() {
+		System.out.println("I do all sorts of things to annoy the neighbours.");
+	}
+	
+}

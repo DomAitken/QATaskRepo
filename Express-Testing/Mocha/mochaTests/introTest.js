e07c9af786e7253ef8dd8b1a0d529aef2908f359
DomAitken <daitken123@hotmail.com>
2022-01-10 15:13:21 +0000

    Mocha Testing Intro

--- Express-Testing/Mocha/mochaTests/introTest.js
+++ Express-Testing/Mocha/mochaTests/introTest.js
@@ -1,9 +1,31 @@
-describe('My first group of tests', function() {
-    it('Should print the word hello', function() {
+// Mocha tests are designated with the following:
+// describe() - Test Suite, groups of tests together 
+// it() - An individual test
+
+// describe('Message of what the group is', whatYoureTesting())
+describe('My first tests', function() {
+
+    // it('Message of the test', whatYouReTesting())
+    it('Should print the words hello world', function() {
         console.log("Hello World!");
     });
+
+    it('will print the number 5', function() {
+        console.log(5);
+    })
+
 });
 
-//To run mocha tests:
-// 'mocha <name of test folder>'
-// 'mocha mochaTests'
+describe('Second group of tests', function() {
+    it('Will print Hello World', function() {
+        console.log("Hello World");
+    })
+});
+
+// To run mocha tests 
+// `mocha <name of test folder>`
+// `mocha mocha-tests`
+
+// Exercise - Create a tests folder
+// - Create a test file
+// In the test file create a describe test group and a it test just printing out "Hello World"

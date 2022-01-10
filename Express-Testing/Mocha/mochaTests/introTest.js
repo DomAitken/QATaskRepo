a8be8a9fd6544a8100033198b3db8aae83e41c41
DomAitken <daitken123@hotmail.com>
2022-01-10 12:50:17 +0000

    First Mocha tests

---
+++ Express-Testing/Mocha/mochaTests/introTest.js
@@ -0,0 +1,9 @@
+describe('My first group of tests', function() {
+    it('Should print the word hello', function() {
+        console.log("Hello");
+    });
+});
+
+//To run mocha tests:
+// 'mocha <name of test folder>'
+// 'mocha mochaTests'

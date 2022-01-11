3f24019b799ccf01a88cbcb0bc83c979ee4878f7
DomAitken <daitken123@hotmail.com>
2022-01-11 20:01:52 +0000

    App.js Updated for Contact Card Task

--- React/bae-react/src/App.js
+++ React/bae-react/src/App.js
@@ -3,16 +3,17 @@ import CompWithVars from './components/components-vars/CompWithVars';
 import Page from './components/Parent-ChildTask/page';
 import Parent from './components/Parent-Child/Parent';
 import Pizza from './components/Props/Pizza';
+import PizzaOrder from './components/Props/PizzaOrder';
 import './css/App.css';
 // import CompWithVars from './components/component-vars/CompWithVars.jsx';
 // import HelloWorld from './components/HelloWorld.jsx';
-import PizzaOrder from './components/Props/PizzaOrder';
+import ContactCardManager from './components/ContactCard-Task/ContactCardManager';
 
 function App() {
     return (<div className = "App">
         {/* <HelloWorld/>
         <CompWithVars/> */}
-        <PizzaOrder/>
+        <ContactCardManager/>
     </div>
     );
 }

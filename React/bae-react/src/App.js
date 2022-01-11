ba253dd940feb05990c8b83df8aa7cd637af6e1c
DomAitken <daitken123@hotmail.com>
2022-01-11 20:03:11 +0000

    App.js Updated.

--- React/bae-react/src/App.js
+++ React/bae-react/src/App.js
@@ -1,9 +1,9 @@
-import HelloWorld from './components/HelloWorld';
-import CompWithVars from './components/components-vars/CompWithVars';
-import Page from './components/Parent-ChildTask/page';
-import Parent from './components/Parent-Child/Parent';
-import Pizza from './components/Props/Pizza';
-import PizzaOrder from './components/Props/PizzaOrder';
+// import HelloWorld from './components/HelloWorld';
+// import CompWithVars from './components/components-vars/CompWithVars';
+// import Page from './components/Parent-ChildTask/page';
+// import Parent from './components/Parent-Child/Parent';
+// import Pizza from './components/Props/Pizza';
+// import PizzaOrder from './components/Props/PizzaOrder';
 import './css/App.css';
 // import CompWithVars from './components/component-vars/CompWithVars.jsx';
 // import HelloWorld from './components/HelloWorld.jsx';

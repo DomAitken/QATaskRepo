d19d3db329f9ae10a1caacbd5f3621c996cdf096
DomAitken <daitken123@hotmail.com>
2022-01-11 12:49:01 +0000

    Hello World! file

--- React/bae-react/src/components/HelloWorld.jsx
+++ React/bae-react/src/components/HelloWorld.jsx
@@ -1,7 +1,10 @@
-const helloWorld = () => {
+const HelloWorld = () => {
     return ( 
-        <h3> Hello World! </h3>
+        [<h3> Hello World! </h3>,
+        <h3>I'm new to this.</h3>,
+        <h4>Please send help.</h4>,
+        <h4>Preferably someone good at coding.</h4>]
     )
 };
 
-export default helloWorld;
+export default HelloWorld;

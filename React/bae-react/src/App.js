c906676e92d3fd2a54f7c415f66cae3c48af1610
DomAitken <daitken123@hotmail.com>
2022-01-11 20:00:39 +0000

    App Updated for Pizza Task

--- React/bae-react/src/App.js
+++ React/bae-react/src/App.js
@@ -1,15 +1,20 @@
+import HelloWorld from './components/HelloWorld';
+import CompWithVars from './components/components-vars/CompWithVars';
+import Page from './components/Parent-ChildTask/page';
+import Parent from './components/Parent-Child/Parent';
+import Pizza from './components/Props/Pizza';
 import './css/App.css';
 // import CompWithVars from './components/component-vars/CompWithVars.jsx';
 // import HelloWorld from './components/HelloWorld.jsx';
-import Footer from './components/Parent-ChildTask/footer';
+import PizzaOrder from './components/Props/PizzaOrder';
 
-function helloWorld() {
+function App() {
     return (<div className = "App">
         {/* <HelloWorld/>
         <CompWithVars/> */}
-        <Footer/>
+        <PizzaOrder/>
     </div>
     );
 }
 
-export default helloWorld;
+export default App;

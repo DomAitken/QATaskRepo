4f7746a8dbc0cd033f7b3511aa613d97bea7b82f
DomAitken <daitken123@hotmail.com>
2022-01-11 16:19:00 +0000

    Pizza task Files

---
+++ React/bae-react/src/components/Props/PizzaOrder.jsx
@@ -0,0 +1,12 @@
+import Pizza from "./Pizza";
+
+const PizzaOrder = () => {
+    return ( 
+        <div>
+            <Pizza name="Dom" size="12" flavour="Pepperoni"/>
+            <Pizza name="Matt" size="12" flavour="Ham and Olive"/>
+        </div>
+    ); 
+}
+ 
+export default PizzaOrder;

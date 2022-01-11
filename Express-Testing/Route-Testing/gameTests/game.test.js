eebecfd73707365f4231db88c13a9ec166908cbb
DomAitken <daitken123@hotmail.com>
2022-01-11 09:30:50 +0000

    Updates to tests

--- Express-Testing/Route-Testing/gameTests/game.test.js
+++ Express-Testing/Route-Testing/gameTests/game.test.js
@@ -6,6 +6,13 @@ const server = require('../index.js')
 chai.use(chaiHttp);
 
 describe('Route testing', function() {
+    const testGame = {
+        gameName: "testName",
+        genre: "testGenre",
+        price: 5,
+        isGood: true
+    }
+
     it('Should respond with "Test path successful"', function(done) {
         chai.request(server)
             .get('/games/test')
@@ -19,5 +26,41 @@ describe('Route testing', function() {
                 expect(res).to.have.property('text', "Test path successful");
                 done();
             });
-    })
+    });
+
+    it('Should post data to the database and return <name> added to the database', function(done) {
+        chai.request(server)
+            .post('/games/post')
+            .send(testGame)
+            .end((err, res) => {
+                if (err) {
+                    console.log("Error ocurred.");
+                    done(err);
+                };
+                expect(res).to.have.status(201);
+                expect(res).to.not.be.null;
+                expect(res).to.have.property('text', `${testGame.gameName} added to database.`);
+                done();
+            })
+    });
+
+    it('Should return all games from the database', function(done) {
+        chai.request(server)
+            .get('/games/getAll')
+            .end((err, res) => {
+                if (err) {
+                    console.log("Error ocurred.");
+                    done(err);
+                };
+
+                const resBody = res.body;
+                expect(res).to.have.status(200);
+                expect(resBody).to.not.be.null;
+                resBody.map((games) => {
+                    expect(games).to.be.a.Arguments("Object");
+                    expect(games).to.contain.keys("gameName");
+                });
+                done();
+            });
+    });
 })

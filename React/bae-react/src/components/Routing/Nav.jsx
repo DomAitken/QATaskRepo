cab5475d8efcf1483750019a5921aa3a685dcc7f
DomAitken <daitken123@hotmail.com>
2022-01-13 14:40:51 +0000

    Routing Files

--- React/bae-react/src/components/Routing/Nav.jsx
+++ React/bae-react/src/components/Routing/Nav.jsx
@@ -1,9 +1,35 @@
+import {Link} from 'react-router-dom';
+
 const Nav = () => {
     return ( 
         <div>
             <h3>Nav</h3>
+            <Link to="/about">
+                <h4>About</h4>
+                {/* <button type='button'>About</button> */}
+            </Link>
+
+            <Link to="/shop">
+                <h4>Shop</h4>
+                {/* <button type='button'>Shop</button> */}
+            </Link>
+
+            <Link to="/">
+                <h4>Home</h4>
+                {/* <button type='button'>Home</button> */}
+            </Link>
+
+            <Link to="/forum">
+                <h4>Forum</h4>
+                {/* <button type='button'>Forum</button> */}
+            </Link>
+
+            <Link to="/checkout">
+                <h4>Checkout</h4>
+                {/* <button type='button'>Checkout</button> */}
+            </Link>
         </div>
-     );
+    );
 }
- 
+
 export default Nav;

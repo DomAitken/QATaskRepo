50d48543cefc6e86f73034efa68aec6743c02d22
DomAitken <daitken123@hotmail.com>
2022-01-13 16:07:04 +0000

    Updated App.js

--- React/bae-react/src/App.js
+++ React/bae-react/src/App.js
@@ -24,7 +24,7 @@ import Forum from './components/Routing/Forum';
 import Checkout from './components/Routing/Checkout';
 import Child from './components/Routing/ChildModule';
 // import HookParent from './components/useEffect/Tasks/useEffect-Task/HookParent';
-
+import Bar from './components/RequestHandling/Bar';
 
 function App() {
     return (<div className = "App">
@@ -62,6 +62,7 @@ function App() {
             {/* </Routes> */}
             {/* <Footer/> */}
         {/* </BrowserRouter> */}
+        <Bar/>
     </div>
     );
 }

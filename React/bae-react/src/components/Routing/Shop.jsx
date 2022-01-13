0123aab9e9ff040a3b1df5957e59bcfc74438764
DomAitken <daitken123@hotmail.com>
2022-01-13 14:43:52 +0000

    Routing Files

--- React/bae-react/src/components/Routing/Shop.jsx
+++ React/bae-react/src/components/Routing/Shop.jsx
@@ -1,9 +1,14 @@
+import { useParams } from "react-router-dom";
+
 const Shop = () => {
+
+    const{id} = useParams();
+
     return ( 
         <div>
-            <h3>Shop</h3>
+            <h3>Shop page of id: {id}</h3>
         </div>
-     );
+    );
 }
- 
+
 export default Shop;

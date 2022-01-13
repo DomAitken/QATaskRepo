2f5e58c27c4bbeb98bd90faf1108bfc2da01229a
DomAitken <daitken123@hotmail.com>
2022-01-13 12:26:26 +0000

    Updated App.js

--- React/bae-react/src/App.js
+++ React/bae-react/src/App.js
@@ -13,7 +13,15 @@ import './css/App.css';
 // import Manager from './components/State/Lifting-State/Manager';
 // import Clock from './components/useEffect/Clock';
 // import HideText from './components/useEffect/Tasks/Read-More/ReadMoreButton';
-import UseEffectTask from './components/useEffect/Tasks/useEffect-Task/useEffectTask';
+// import UseEffectTask from './components/useEffect/Tasks/useEffect-Task/useEffectTask';
+import Nav from './components/Routing/Nav';
+import Footer from './components.Routing/Footer';
+import Home from './components/Routing/Home';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import About from './components/Routing/About';
+import Shop from './components/Routing/Shop';
+
+
 
 function App() {
     return (<div className = "App">
@@ -31,7 +39,24 @@ function App() {
         {/* <Manager/> */}
         {/* <Clock/> */}
         {/* <HideText/> */}
-        <UseEffectTask/>
+        {/* <UseEffectTask/> */}
+        <Router>
+            <Nav/>
+            <Routes>
+                <Route path="/" exact>
+                    <Home/>
+                </Route>
+
+                <Route path="/about" exact>
+                    <About/>
+                </Route>
+
+                <Route path="/shop" exact>
+                    <Shop/>
+                </Route>
+            </Routes>
+            <Footer/>
+        </Router>
     </div>
     );
 }
